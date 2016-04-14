2b7c1d93bc6c2441dda17b681e5860247a5c6a83
Luphia Chang <luphia@outlook.com>
2016-04-14 18:03:04 +0800

    Release v1.0.4

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var reg = {};
-reg.publicIP = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+reg.IP = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
 reg.email = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 reg.number = /^-?\d+\.?\d*$/;
 reg.json = /^{}$|^\[\]$/;
+reg.internalIP = /(^127\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)/;
 
 var textype = function () {};
+textype.isIP = function (text) {
+	return reg.IP.test(text);
+};
 textype.isPublicIP = function (text) {
-	return reg.publicIP.test(text);
+	return reg.IP.test(text) && !reg.internalIP.test(text);
+};
+textype.isInternalIP = function (text) {
+	return reg.IP.test(text) && reg.internalIP.test(text);
 };
 textype.isEmail = function (text) {
 	return reg.email.test(text);
